38f2fa5044d86ae02859e8309e63f848e51e6078
Nguyenduc2108 <vanducnguyen2108@gmail.com>
2024-03-20 03:33:36 +0700

    update server 3:33am 20/03/2024

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./src/config/connectDB");
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.URL_CLIENT,
+  })
+);
+
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+
+// test server
+app.use("/", (req, res) => {
+  res.send("app run");
+});
+connectDB();
+
+const port = process.env.PORT || 8888;
+
+app.listen(port, () => {
+  console.log(`Server is running on the port ` + port);
+});
