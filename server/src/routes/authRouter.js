56a92429afb663cadb3fc038cc62fc8dce8ef749
Nguyenduc2108 <vanducnguyen2108@gmail.com>
2024-05-03 08:18:55 +0700

    update

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -27,29 +27,6 @@ router.get(
     }
 );
 
-router.get(
-    "/facebook",
-    passport.authenticate("facebook", {
-        scope: ["email"],
-        session: false,
-    })
-);
-
-router.get(
-    "/facebook/callback",
-    (req, res, next) => {
-        passport.authenticate("facebook", (err, profile) => {
-            req.user = profile;
-            next();
-        })(req, res, next);
-    },
-    (req, res) => {
-        res.redirect(
-            `${process.env.URL_CLIENT}/login-success/${req.user?.id}/${req.user.tokenLogin}`
-        );
-    }
-);
-
 router.post("/login-success", authController.loginSuccess);
 
 router.post("/register", async (req, res) => {
